c5b270857f849214789b296f6f1ff8881c99bab0
Spencer Hachmeister <tms@spencerslab.com>
2020-05-27 21:20:05 -0600

    Add comments on adding css classses to the label field.

--- inventorytracker/api/forms.py
+++ inventorytracker/api/forms.py
@@ -1,6 +1,15 @@
 from django.forms import ModelForm, TextInput, TimeInput
 from api.models import *
 
+## This is a reference/experiment for how to add css classes to the label fields. 
+#class BaseForm(ModelForm):
+#    def __init__(self, *args, **kwargs):
+#        super(BaseForm, self).__init__(*args, **kwargs)
+#        #print(self.as_table())
+#        for field in self: 
+#            field.label_classes = ("lc")
+#            print(field.label_tag(attrs={"class": "woot"}))
+
 class SensorModelForm(ModelForm):
     class Meta:
         model = Sensor
